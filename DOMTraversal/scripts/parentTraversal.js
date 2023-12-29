bb5bfe8ffd1efc921340cebfd5a07e736fb40a3d
Yatesh Gumma <yateshy38@gmail.com>
2023-12-28 20:08:33 -0500

    DOM Traversal

---
+++ DOMTraversal/scripts/parentTraversal.js
@@ -0,0 +1,4 @@
+const li = document.querySelector('li');
+
+console.log(li.parentElement);
+console.log(li.parentNode);
---
+++ DOMTraversal/scripts/sibilingTraversal.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector('ul');
+
+console.log(ul.nextElementSibling);
+console.log(ul.nextSibling);
+
+console.log(ul.previousElementSibling);
+console.log(ul.previousSibling);
